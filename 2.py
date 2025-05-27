31aaedeca9043b535595d332115597ebacf2b2f6
Madhur09M <117634573+Madhur09M@users.noreply.github.com>
2025-05-27 09:00:44 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,17 @@
+name = 'Madhur'
+age = 21
+weight = 79.45
+
+print('name')
+print(name)
+
+print(age)
+print(weight)
+
+print("My name is : ", name)
+
+print (age , weight)
+
+print(type(name))
+print(type(age))
+print(type(weight))
---
+++ 4practice.py
@@ -0,0 +1,24 @@
+#take input of two num and add them
+'''
+a = int(input("Enter the 1st number = "))
+b = int(input("Enter the 2nd number = "))
+
+sum = a+b
+
+print(sum)'''
+
+#take input a side of a square and print its area
+'''
+s = float(input("Enter the side of a Square :"))
+
+print("Area of a Square : ",s*s)'''
+
+
+#take input of two float numbers and print there avg
+
+p = float(input("Enter the value of 1st number : "))
+q = float(input("Enter the value of 2nd number : "))
+
+r = (p+q)/2
+
+print("The avg of two numbers is : ",r)
