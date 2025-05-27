a4c6c02329871f8d03e4db071910cd2b9bf8e009
Madhur09M <117634573+Madhur09M@users.noreply.github.com>
2025-05-27 09:07:37 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
-print('hello world')
-print("23")
-print(75)
+#printing the string and the integer value on the output screen
+
+print('hello world')  #string
+print("23")           #string
+print(75)             #integer
