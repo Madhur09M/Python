b211559ee31d4a31240ccef44761be683b6fb685
Madhur09M <117634573+Madhur09M@users.noreply.github.com>
2025-05-29 10:14:37 +0530

    Add files via upload

---
+++ 8tuple.py
@@ -0,0 +1,14 @@
+#wap to count the number of students with grade 'A' in the followinf tuple
+# tup = ("C","D","A","A","B","D","A")
+
+tup = ("C","D","A","A","B","D","A")
+
+n = tup.count("A")
+print("Number of students with 'A' grade :",n)
+
+#store the above values in a list and sort them from "A" TO "D"
+
+list1 = ["C","D","A","A","B","D","A"]
+
+list1.sort()
+print(list1)
